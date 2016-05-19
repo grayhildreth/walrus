4b25899fdcb3e8f8ca76fc52c98c890c2f622447
Carlos Urquiza and Gray Hildreth <webquiza@gmail.com>
2016-05-18 20:49:17 -0700

    It will add buttons to change background color using bootstrap, and jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,21 @@ $(document).ready(function() {
     $(".initially-hidden").fadeToggle();
   });
 });
+
+
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+});
